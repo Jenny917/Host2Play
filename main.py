2eacb3bb1501163b9e0a6a5e137a2b2baf91de3c
Jenny917 <125307426+Jenny917@users.noreply.github.com>
2026-05-05 20:26:24 +0800

    Update RENEW_URLS with new URL

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=c338b3ac-c502-42ac-8f14-e390e9f56240",
     # 添加更多链接
 ]
 
